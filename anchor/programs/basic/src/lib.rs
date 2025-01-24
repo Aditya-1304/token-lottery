c9081e67e976467db669d3838328eb1e608a45f6
Aditya Mandal <mandaladitya1614@gmail.com>
2025-01-24 21:41:43 +0530

    Initialize_lottery commit

--- anchor/programs/basic/src/lib.rs
+++ anchor/programs/basic/src/lib.rs
@@ -1,11 +1,21 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{associated_token::AssociatedToken, metadata::Metadata, token_interface::{Mint, TokenAccount, TokenInterface}};
+use anchor_spl::{associated_token::AssociatedToken, metadata::Metadata, token_interface::{MintTo,mint_to,Mint, TokenAccount, TokenInterface}};
+use anchor_spl::metadata::{create_metadata_accounts_v3, CreateMetadataAccountsV3, mpl_token_metadata::types::{Creator, DataV2}};
 
 declare_id!("6z68wfurCMYkZG51s1Et9BJEd9nJGUusjHXNt4dGbNNF");
 
+#[constant]
+pub const NAME : &str = "Token Lottery Ticket #";
+#[constant]
+pub const SYMBOL : &str = "TLT";
+
+#[constant]
+pub const URI : &str = "https://raw.githubusercontent.com/solana-developers/developer-bootcamp-2024/refs/heads/main/project-9-token-lottery/metadata.json";
 #[program]
 pub mod basic {
 
+
+    use anchor_spl::metadata::{create_master_edition_v3, mpl_token_metadata::types::CollectionDetails, sign_metadata, CreateMasterEditionV3, SignMetadata};
 
     use super::*;
 
@@ -22,6 +32,94 @@ pub mod basic {
     }
 
     pub fn initialize_lottery(ctx: Context<InitializeLottery>) -> Result<()> {
+
+        let signer_seeds : &[&[&[u8]]] = &[&[
+            b"collection_mint".as_ref(),
+            &[ctx.bumps.collection_mint]
+        ]];
+
+        msg!("Creating Mint account");
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    mint: ctx.accounts.collection_mint.to_account_info(),
+                    to: ctx.accounts.collection_token_account.to_account_info(),
+                    authority: ctx.accounts.collection_mint.to_account_info(),
+                },
+                signer_seeds,
+                
+            ),
+            1
+        )?;
+
+        msg!("Creating Metadata account");
+        create_metadata_accounts_v3(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_metadata_program.to_account_info(),
+                CreateMetadataAccountsV3{
+                    metadata: ctx.accounts.metadata.to_account_info(),
+                    mint: ctx.accounts.collection_mint.to_account_info(),
+                    mint_authority: ctx.accounts.collection_mint.to_account_info(),
+                    payer: ctx.accounts.payer.to_account_info(),
+                    update_authority: ctx.accounts.collection_mint.to_account_info(),
+                    system_program : ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+                &signer_seeds,
+            ),
+            DataV2{
+                name: NAME.to_string(),
+                symbol: SYMBOL.to_string(),
+                uri: URI.to_string(),
+                seller_fee_basis_points: 0,
+                creators: Some(vec![Creator {
+                    address: ctx.accounts.collection_mint.key(),
+                    verified : false,
+                    share: 100,
+                }]),
+                collection:None,
+                uses: None,
+            },
+            true,
+            true,
+            Some(CollectionDetails::V1 {size: 0})
+
+        )?;
+
+        msg!("Creating Master Edition account");
+        create_master_edition_v3(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_metadata_program.to_account_info(), 
+                CreateMasterEditionV3 {
+                    payer : ctx.accounts.payer.to_account_info(),
+                    mint: ctx.accounts.collection_mint.to_account_info(),
+                    edition: ctx.accounts.master_edition.to_account_info(),
+                    mint_authority: ctx.accounts.collection_mint.to_account_info(),
+                    update_authority: ctx.accounts.collection_mint.to_account_info(),
+                    metadata: ctx.accounts.metadata.to_account_info(),
+                    token_program: ctx.accounts.token_program.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                }, 
+                signer_seeds),
+            Some(0)
+        
+        )?;
+
+        msg!("Verifying collection");
+        sign_metadata(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_metadata_program.to_account_info(), 
+                SignMetadata {
+                    creator: ctx.accounts.collection_mint.to_account_info(),
+                    metadata: ctx.accounts.metadata.to_account_info(),
+                }, 
+                signer_seeds
+            ))?;
+
+
+
         Ok(())
     }
 }
@@ -70,10 +168,30 @@ pub struct InitializeLottery<'info> {
     )]
     pub collection_token_account: InterfaceAccount<'info, TokenAccount>,
 
+    #[account(
+        mut,
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), collection_mint.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key(),
+    )]
+    /// CHECK:This account is checked by the metadata smart contract
+    pub metadata: UncheckedAccount<'info>,
+
+    
+    #[account(
+        mut,
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), collection_mint.key().as_ref(),b"edition"],
+        bump,
+        seeds::program = token_metadata_program.key()
+    )]
+    /// CHECK:This account is checked by the metadata smart contract
+    pub master_edition: UncheckedAccount<'info>,
+
     pub token_metadata_program : Program<'info, Metadata>,
     pub associate_token_program : Program<'info, AssociatedToken>,
     pub token_program : Interface<'info, TokenInterface>,
     pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
 } 
 
 #[account]
--- anchor/target/types/basic.ts
+++ anchor/target/types/basic.ts
@@ -164,6 +164,84 @@ export type Basic = {
           }
         },
         {
+          "name": "metadata",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  109,
+                  101,
+                  116,
+                  97,
+                  100,
+                  97,
+                  116,
+                  97
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "tokenMetadataProgram"
+              },
+              {
+                "kind": "account",
+                "path": "collectionMint"
+              }
+            ],
+            "program": {
+              "kind": "account",
+              "path": "tokenMetadataProgram"
+            }
+          }
+        },
+        {
+          "name": "masterEdition",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  109,
+                  101,
+                  116,
+                  97,
+                  100,
+                  97,
+                  116,
+                  97
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "tokenMetadataProgram"
+              },
+              {
+                "kind": "account",
+                "path": "collectionMint"
+              },
+              {
+                "kind": "const",
+                "value": [
+                  101,
+                  100,
+                  105,
+                  116,
+                  105,
+                  111,
+                  110
+                ]
+              }
+            ],
+            "program": {
+              "kind": "account",
+              "path": "tokenMetadataProgram"
+            }
+          }
+        },
+        {
           "name": "tokenMetadataProgram",
           "address": "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
         },
@@ -177,6 +255,10 @@ export type Basic = {
         {
           "name": "systemProgram",
           "address": "11111111111111111111111111111111"
+        },
+        {
+          "name": "rent",
+          "address": "SysvarRent111111111111111111111111111111111"
         }
       ],
       "args": []
@@ -245,6 +327,23 @@ export type Basic = {
           }
         ]
       }
+    }
+  ],
+  "constants": [
+    {
+      "name": "name",
+      "type": "string",
+      "value": "\"Token Lottery Ticket #\""
+    },
+    {
+      "name": "symbol",
+      "type": "string",
+      "value": "\"TLT\""
+    },
+    {
+      "name": "uri",
+      "type": "string",
+      "value": "\"https://imgs.search.brave.com/m1cUYln9TPVNmvpm-fGvjKMGblfvQd9gl1zR8a6DCxk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAxLzA5LzE0LzAy/LzM2MF9GXzEwOTE0/MDI2OF9pcGNiTjZB/cUhqdnJKblNrOFU2/andSZHdIMzBzYUYw/OC5qcGc\""
     }
   ]
 };
